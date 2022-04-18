48aa0a192fb301defd39e511b8442c56e850855d
ibrmoh <you@example.com>
2022-04-18 23:33:19 +0300

    newtask

--- routes/web.php
+++ routes/web.php
@@ -23,25 +23,6 @@ Route::get('/about', function () {
     return view("about",compact('tasks'));
 });
 
-Route::get('/contact', function () {
-    $name = "Mohammed";
-    $age = 30;
-    return view ('contact', compact('name','age'));
-});
-Route::get('/show/{id}', function ($id) {
-    $tasks = [
-        "1" => "task 1",
-        "2" => "task 2",
-        "3" => "task 3"
-     ];
-     $task = $tasks[$id];
-    return view('show',compact('task'));
-});
-Route::post('/show', function () {
-    $name = $_POST['name'];
-    return view('show',compact('name'));
-});
-// Route::post('/about', function () {
-//     $name = $_POST['name'];
-//     return view('about',compact('name'));
-// });
+
+
+
